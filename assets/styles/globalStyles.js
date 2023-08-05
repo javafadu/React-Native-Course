86afba7d391d7f7a6c4f790e6978f5f0a5326f2a
Feridun Ozcelik <101672110+javafadu@users.noreply.github.com>
2023-08-05 20:09:58 +0300

    Add files via upload

---
+++ assets/styles/globalStyles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    greenBox: {
+        backgroundColor: "green",
+        padding: 30,
+        borderRadius: 10
+    }
+});
+
+export default styles;
